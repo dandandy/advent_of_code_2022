108ee04ce3ddbaf9001fb88c29d5534ac3e26433
Daniel Barrett <dbarrett@atlassian.com>
2022-12-06 16:48:32 +1100

    use list comprehensions

--- src/Day6.hs
+++ src/Day6.hs
@@ -3,14 +3,11 @@ module Day6 where
 import Data.List
 import Text.Parsec
 
-areAllUnique :: String -> Bool
-areAllUnique s = length s == length (nub s)
-
-window :: (String -> Bool) -> Int -> String  -> (String,Int)
-window p size input = head $ filter (p . fst) $ map (\a -> (slice a (a+size - 1) input, a + size)) [0..(length input - size - 1)]
-
-slice :: Int -> Int -> [a] -> [a]
-slice from to xs = take (to - from + 1) (drop from xs)
+part12 :: Int -> String -> Int 
+part12 len str  = head $ [i+ len  | i <- [0..length str - len], length ( nub $ slice i (i+len) str) == len]
 
 parser :: Parsec String u String
 parser = many1 letter
+
+slice :: Int -> Int -> [a] -> [a]
+slice a b ls = map snd $ filter ((\v -> v>=a && v<b) . fst) $ zip [0..] ls 
