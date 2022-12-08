c70d7e9f62b684d8365e372ad6105220c07f6c96
Daniel Barrett <dbarrett@atlassian.com>
2022-12-08 15:34:19 +1100

    passes day7 example, slow on main

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,7 +1,23 @@
 module Day7 where
 import Text.Parsec
+    ( char,
+      digit,
+      letter,
+      newline,
+      space,
+      string,
+      many1,
+      sepEndBy,
+      (<|>),
+      try,
+      Parsec )
+import Control.Monad.State (State, get, put, runState)
+import Data.Map (Map, mapWithKey, empty, insert, toList, map)
+import Data.Maybe (mapMaybe)
+import qualified Control.Applicative
+import Data.List (subsequences, maximumBy, map)
 
-data FileTree = FileTree String [(Int, String)] [FileTree] deriving (Show)
+data Stack a = Stack [a] deriving (Show)
 
 data Cmd = Cd String | Ls [LsOutput] deriving (Show)
 
@@ -31,21 +47,51 @@ parseFileLine = pure File <*> (parseInt <* space) <*> parseFileName
         parseInt :: Parsec String String Int
         parseInt = read <$> many1 digit
 
-buildTree :: [Cmd] -> FileTree
-buildTree ((Cd "/"):cmd) = buildTree' (FileTree "/" [] []) cmd
-buildTree _ = error "needs to start from /"
+runState' :: [Cmd] -> ([()], (Stack String, Map String [(Int, String)]))
+runState' c = runState (mapM toInfo c) (Stack [], empty)
+    where
+        toInfo :: Cmd -> State (Stack String, Map String [(Int, String)]) ()
+        toInfo cmd = do (stack, files) <- get
+                        put (applyCmdToStack cmd stack, applyCmdToMap cmd stack files)
+                        return ()
 
-buildTree' :: FileTree -> [Cmd] -> FileTree
-buildTree' f [] = f
-buildTree' f ((Cd ".."):cmd) = f   
-buildTree' f ((Cd "/"):cmd) = error "cannot return to root"   
-buildTree' (FileTree name files dirs) ((Cd dir):cmd) = FileTree name files ((buildTree' (head $ filter ((==dir) . (\(FileTree name _ _) -> name)) dirs ) cmd):(filter ((/=dir) . (\(FileTree name _ _) -> name)) dirs))
-buildTree' (FileTree name files dirs) ((Ls lsOutput):cmd) = buildTree' (FileTree name (files ++ concatMap toFile lsOutput) (dirs ++ concatMap toDir lsOutput)) cmd
 
-toFile :: LsOutput -> [(Int, String)]
-toFile (File a b) = [(a, b)]
-toFile (Dir _) = []
+run :: [Cmd] -> Int
+run cmds = sum . Data.List.map (snd) $ largestDirsWithAtMost 100000 (sum' map)
+    where
+        (_,(_, map)) = runState' cmds
 
-toDir :: LsOutput -> [FileTree]
-toDir (File _ _) = []
-toDir (Dir a) = [FileTree a [] []]
+        sum' :: Data.Map.Map String [(Int, String)] -> Data.Map.Map String Int
+        sum' = Data.Map.map (sum . Data.List.map fst)
+
+largestDirsWithAtMost :: Int -> Map String Int -> [(String, Int)]
+largestDirsWithAtMost n = maximumBy compare' . filterLessEqN . filter (not . null) . subsequences  . toList
+    where
+        filterLessEqN :: [[(String, Int)]] -> [[(String, Int)]]
+        filterLessEqN = filter ((<=n) . sum')
+
+        compare' :: [(String, Int)] -> [(String, Int)] -> Ordering
+        compare' as bs = compare (sum' as) (sum' bs)
+
+        sum' :: [(String, Int)] -> Int
+        sum' = sum . Data.List.map snd
+
+applyCmdToStack :: Cmd -> Stack String -> Stack String
+applyCmdToStack (Cd "..") (Stack s) = Stack $ tail s
+applyCmdToStack (Cd a) (Stack s) = Stack $ a:s
+applyCmdToStack _ s = s
+
+applyCmdToMap :: Cmd -> Stack String -> Map String [(Int, String)] -> Map String [(Int, String)]
+applyCmdToMap (Cd "..") _ m = m 
+applyCmdToMap (Cd a) _ m = insert a [] m
+applyCmdToMap (Ls output) (Stack stack) m = mapWithKey (\k v -> if k `elem` stack then v ++ lsOutput else v) m
+    where 
+        lsOutput = mapMaybe toFile output
+
+toFile :: LsOutput -> Maybe (Int, String)
+toFile (File a b) = Just (a, b)
+toFile (Dir _) = Nothing
+
+toDir :: LsOutput -> Maybe String
+toDir (File _ _) = Nothing
+toDir (Dir a) = Just a
