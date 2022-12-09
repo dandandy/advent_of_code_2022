69f75b72d5265985504336c329c6e158b5ab6a59
Daniel Barrett <dbarrett@atlassian.com>
2022-12-09 13:47:20 +1100

    day 8 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -3,7 +3,7 @@ module Day8 where
 import Data.Matrix
 import Text.Parsec
 import Data.List (sort, length, intercalate)
-import Data.Vector (splitAt, Vector, toList, length, take, drop)
+import Data.Vector (splitAt, Vector, toList, length, take, drop, takeWhile, reverse)
 import qualified Data.Vector.Primitive as Data
 import qualified Debug.Trace as Debug
 
@@ -13,8 +13,8 @@ parse' = fromLists <$> (many1 (read . (:[]) <$> digit) `sepEndBy` newline)
 count' :: Matrix Bool -> Int
 count' = Data.List.length . filter (==True) . Data.Matrix.toList
 
-highLight :: Matrix Int -> Matrix Bool
-highLight m = Data.Matrix.mapPos (\pos _ -> isVisable pos m) m
+highLight :: Matrix Int -> Matrix [[Int]]
+highLight m = Data.Matrix.mapPos (\pos _ -> visableFrom pos m) m
 
 isVisable :: (Int, Int) -> Matrix Int -> Bool
 -- isVisable (1, b) m = True
@@ -26,12 +26,37 @@ isVisable (a', b') m = isBigger' (start b row) || isBigger' (end b row) || isBig
         b=b' - 1
         isBigger' = isBigger el
         el = getElem a' b' m
-        row = Debug.trace ("row " ++ intercalate " " [show a])  getRow a' m
-        col = Debug.trace ("col " ++ intercalate " " [show b])  getCol b' m
+        row = getRow a' m
+        col = getCol b' m
+
+visableFrom :: (Int, Int) -> Matrix Int -> [[Int]]
+visableFrom (a', b') m = [canSee' (start b row),canSee' (end b row), canSee' (start a col), canSee' (end a col)]
+    where
+        a = a' - 1
+        b = b' - 1
+        canSee' = canSee el
+        el = getElem a' b' m
+        row = getRow a' m
+        col = getCol b' m
         
-start a v = Debug.trace  ("start: " ++ unwords [show a, show v]) (Data.Vector.take a v)
-end a v = Debug.trace ("end: " ++ unwords [show a, show v]) ((Data.Vector.drop . (+1)) a v)
+start a v = Debug.trace  ("start: " ++ unwords [show a, show v]) ((Data.Vector.reverse .  Data.Vector.take a) v)
+end a v = (Data.Vector.drop (a+1))  v
 
 isBigger :: Int -> Data.Vector.Vector Int -> Bool
 isBigger x xs   | Data.Vector.length xs == 0  = True 
                 | otherwise             = Debug.trace ("isBigger: " ++ show x ++ ", " ++ show xs) x > maximum xs
+
+canSee :: Int -> Data.Vector.Vector Int -> [Int]
+canSee x = takeTill (<x) (>=x) . Data.Vector.toList 
+
+countTreesCanSee :: [[Int]] -> Int
+countTreesCanSee = product . map Data.List.length
+
+takeTill :: (a -> Bool) -> (a -> Bool) -> [a] -> [a]
+takeTill p specialStop [] = []
+takeTill p specialStop (x:xs)   | specialStop x = [x]
+                                | p x = x:takeTill p specialStop xs
+                                | otherwise = []
+
+runPart2 :: Matrix Int -> Int
+runPart2 = Prelude.maximum . fmap countTreesCanSee <$> highLight
